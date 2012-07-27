6798a85423133af402737e723cd59d917f9958fe
Sebastien Lachance <sebastien.lachance@creatiom.com>
2012-07-27 09:45:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depo::Application.config.secret_token = '567bca20255595997ee5ce9e93b60d7abf260e0e33c0420d7fb40e6e6a245dcee2001a96cf711dd4a3748467ef0192632addfba0c0271c000d8e3641067d2834'
